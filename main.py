ca1eee5ce8f4f6aa12e79987fda4810e0a060675
boyu <oor2020@163.com>
2025-10-04 18:15:58 +0200

    add the function of eliminating redundant edges

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from task1_1.Compute_vector_clocks import compute_vector_clock
-from task1_23.Build_G import build_edges, create_graph
+from task1_2.Build_G import build_edges, create_graph
+from task1_3.reduction_redundant_edges import reduction
 import json
 from networkx.drawing.nx_pydot import write_dot
 
@@ -17,6 +18,11 @@ def main():
     full_edges = build_edges(event_nodes, vector_clocks)
     G = create_graph(full_edges)
     write_dot(G, 'data_vector/full_edges.dot')
+
+    #reduction
+    reduction_edges = reduction(full_edges)
+    G_reduced = create_graph(reduction_edges)
+    write_dot(G_reduced, 'data_vector/reduced_edges.dot')
 
 if __name__ == '__main__':
    main()
